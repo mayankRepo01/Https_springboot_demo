353ebfdd60b269932b23393df809536885b810ce
Your Name <you@example.com>
2022-12-28 12:00:52 +0530

    Https Spring Boot Example

---
+++ src/main/java/com/example/demo/Ssl2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssl2Application.class, args);
+	}
+
+}
